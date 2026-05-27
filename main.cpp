b207cc9e7c2e76e4b70f69b012ee6f5d5c61cc2b
ZxiEcho <jacob.enbjarde@gmail.com>
2026-05-27 15:43:07 +0200

    added some comments

--- main.cpp
+++ main.cpp
@@ -7,23 +7,26 @@ using namespace std;
 #include <SFML/Graphics.hpp>
 
 struct Planets{
-    int id;
+    int id; 
     float X;
     float Y;
     float Z;
-    float size;
-    float mass;
-    float x_speed;
-    float y_speed;
+    float size; //radius of planet
+    float mass; //weight 
+    float x_speed; //speed along the x axis 
+    float y_speed; //speed along the y axis
 };
 
 
 
-bool init(vector<Planets>& planets){
+bool init(vector<Planets>& planets){ //entering all info about the planets 
     int number_planets = 0;
     std::cout << "Enter number of planets" << std::endl;
     std::cin >> number_planets;
-    for(int i = 0; i < number_planets; i++){
+    if(number_planets != 0){ //if we dont have any planets return
+        return false;
+    }
+    for(int i = 0; i < number_planets; i++){ //enter all info for every planet
         Planets temp_planet;
         temp_planet.id = i;
         float x = 0; 
@@ -44,16 +47,13 @@ bool init(vector<Planets>& planets){
         temp_planet.x_speed = xspeed;
         temp_planet.y_speed = yspeed;
 
-        planets.push_back(temp_planet);
+        planets.push_back(temp_planet); //add the planet to our vector with planets
         
-    }
-    if(number_planets != 0){
-        return false;
     }
     return true;
 }
 
-bool init_test(vector<Planets>& planets){
+bool init_test(vector<Planets>& planets){//init with 2 planets for testing
     Planets temp_planet;
     temp_planet.id = 0;
     temp_planet.X = 50;
@@ -62,7 +62,7 @@ bool init_test(vector<Planets>& planets){
     temp_planet.size = 5;
     temp_planet.mass = 5;
     temp_planet.x_speed = 0;
-    temp_planet.y_speed = 0;
+    temp_planet.y_speed = 0.001;
 
     planets.push_back(temp_planet);
 
@@ -80,25 +80,26 @@ bool init_test(vector<Planets>& planets){
     return false;
 }
 
-void update_drawing(vector<Planets>& planets, sf::RenderWindow& window){
+void update_drawing(vector<Planets>& planets, sf::RenderWindow& window){ //render the window every loop itteration
     window.clear();
     for(int i = 0; i < planets.size(); i++){
-        sf::CircleShape shape(planets[i].size);
+        sf::CircleShape shape(planets[i].size); //create the circle 
         shape.setFillColor(sf::Color::Green);
 
-        sf::Vector2f v(planets[i].X, planets[i].Y);
+        sf::Vector2f v(planets[i].X, planets[i].Y); 
+        shape.setPosition(v); //set cirkle position
 
-        shape.setPosition(v);
-
-        window.draw(shape);
+        window.draw(shape); //draw the circles
     }
     window.display();
 }
 
 
 
-vector<float> force_calculator(vector<Planets>& planets){
+vector<float> force_calculator(vector<Planets>& planets){ 
     vector<float> temp_vec = {};
+    float power = 0.00000001; //need to scale the force with this constant so that they dont fly of screen in one loop itteration 
+    float gravity_constant = 6.674 * power;
     for(int i = 0; i < planets.size(); i++){
         for(int j = i + 1; j < planets.size(); j++){
             float xdis = planets[i].X - planets[j].X;
@@ -106,6 +107,10 @@ vector<float> force_calculator(vector<Planets>& planets){
 
             float gravity_x = (planets[i].mass * planets[j].mass) / xdis;
             float gravity_y = (planets[i].mass * planets[j].mass) / ydis;
+
+            gravity_x *= gravity_constant;
+            gravity_y *= gravity_constant;
+
             temp_vec.push_back(gravity_x);
             temp_vec.push_back(gravity_y);
         }
@@ -118,17 +123,29 @@ void apply_force(vector<Planets>& planets, vector<float>& forces){
     for (int i = 0; i < planets.size(); i++){
         for(int j = i + 1; j < planets.size(); j++){ 
             int force_index = i * planets.size() + j - 1;
-            planets[i].x_speed += forces[force_index];  
-            planets[j].x_speed += forces[force_index];
 
-            
-            planets[i].y_speed += forces[force_index + 1];
-            planets[j].y_speed += forces[force_index + 1];
+            if(planets[i].X < planets[j].X){
+                planets[i].x_speed -= forces[force_index];  
+                planets[j].x_speed += forces[force_index];
+            }
+            else{
+                planets[i].x_speed += forces[force_index];  
+                planets[j].x_speed -= forces[force_index];
+            }
+
+            if(planets[i].Y < planets[j].Y) {
+                planets[i].y_speed -= forces[force_index + 1];
+                planets[j].y_speed += forces[force_index + 1];
+            }
+            else{
+                planets[i].y_speed += forces[force_index + 1];  
+                planets[j].y_speed -= forces[force_index + 1];
+            }
         }
     }
 }
 
-void update_position(vector<Planets>& planets){
+void update_position(vector<Planets>& planets){ //updates the position
     for(int i = 0; i < planets.size(); i++){
         planets[i].X += planets[i].x_speed;
         planets[i].Y += planets[i].y_speed;
@@ -143,13 +160,13 @@ int main(int argc, char const *argv[]){
         return 0;
     }
     
-    sf::RenderWindow window(sf::VideoMode({1000,1000}), "SFML Works");
+    sf::RenderWindow window(sf::VideoMode({1000,1000}), "SFML Works"); //creates the window we render to
 
     while(window.isOpen()){
         vector<float> forces = force_calculator(planets);
         apply_force(planets, forces);
+        update_position(planets);
         update_drawing(planets, window);
-        
     }
     
     return 0;
